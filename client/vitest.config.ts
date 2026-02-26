2c7e25f0502a9f9df025b0d4af09049d324925d3
peter <waikatozhang@gmail.com>
2026-02-26 16:38:44 +1300

    fix: fix test error

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,10 +1,9 @@
-import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "./src",
-    plugins: [react(), deno()],
+    plugins: [react()],
     test: {
         environment: "jsdom",
         setupFiles: ["./vitest.setup.ts"],
